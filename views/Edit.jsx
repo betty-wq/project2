f661a0d2b7bc99170c0b6767ba35a6f8589953ee
Betty <futured74@gmail.com>
2020-05-28 09:40:03 -0400

    update

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -10,13 +10,13 @@ class Edit extends React.Component{
                 <a href="/cars/display" class="btn btn-secondary">Return To Display</a>
                 <h1 class="jumbotron">Edit</h1>
                 <form class="forms" action={`/cars/edit/${car._id}?_method=put`} method="POST">
-                      Name:        <input type="text" name="name"/><br/>
-                      Price:       <input type="number" name="price" min="0"/><br/>
-                      Image:        <input type="text" name="img"/><br/>
-                      MPG:          <input type="text" name="mpg"/><br/>
-                      Fuel Type:    <input type="text" name="fuel type"/><br/>
-                      Transmission: <input type="text" name="transmission"/><br/>
-                      Features:     <input type="text" name="features"/><br/>
+                      Name:        <input type="text" name="name" value={car.name}/><br/>
+                      Price:       <input type="number" name="price" min="0" value={car.price}/><br/>
+                      Image:        <input type="text" name="img" value={car.img}/><br/>
+                      MPG:          <input type="text" name="mpg" value={car.mpg}/><br/>
+                      Fuel Type:    <input type="text" name="fuel type" value={car.fuel_type}/><br/>
+                      Transmission: <input type="text" name="transmission" value={car.transmission}/><br/>
+                      Features:     <input type="text" name="features" value={car.features}/><br/>
                       <input type="submit" value="Edit"/>
                       </form>
                       </div>
