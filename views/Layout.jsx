9ed1a4320f07f6f3c084edb6f05736c2b339e80b
Betty <futured74@gmail.com>
2020-05-28 10:10:43 -0400

    update

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -18,7 +18,7 @@ class Layout extends React.Component {
                      {this.props.children}
                     </main>
                     <footer >
-                    <div class="footer-copyright text-center py-3">© 2020 Copyright: Betty
+                    <div class="footer-copyright text-center py-3">© 2020 Copyright:
                      </div>
                     </footer>
                 </body>
