9e573ecff3046d34e136096c14f1f75e39a82c3f
Betty <futured74@gmail.com>
2020-05-28 09:46:42 -0400

    update

--- views/New.jsx
+++ views/New.jsx
@@ -12,13 +12,13 @@ class New extends React.Component {
                <p class="py-2 text-center">"I couldn't find the car of my dreams, so I built it myself." -Ferdinand Porche</p>
                </div>
                    <form action="/cars" method="POST">
-                      Name: <input type="text" name="name"/><br/>
-                      Image: <input type="text" name="img"/><br/>
-                      Price: <input type="text" name="price"/><br/>
-                      MPG: <input type="string" name="mpg"/><br/>
-                      Fuel Type: <input type="text" name="fuel_type"/><br/>
-                      Transmission: <input type="text" name="transmission"/><br/>
-                      Features: <input type="text" name="features"/><br/>
+                      Name: <input class="form-control" type="text" name="name"/><br/>
+                      Image: <input class="form-control" type="text" name="img"/><br/>
+                      Price: <input class="form-control" type="text" name="price"/><br/>
+                      MPG: <input class="form-control" type="string" name="mpg"/><br/>
+                      Fuel Type: <input class="form-control" type="text" name="fuel_type"/><br/>
+                      Transmission: <input class="form-control" type="text" name="transmission"/><br/>
+                      Features: <input class="form-control" type="text" name="features"/><br/>
                       <input type="submit" value="Add"/> 
                    </form>
                    
